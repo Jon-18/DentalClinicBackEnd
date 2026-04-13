bf6a36bc91950f040973f1fc25e0de2e0fb20505
Jonathan Esguerra <78431919+Jon-18@users.noreply.github.com>
2026-04-13 19:29:56 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,17 @@
-// db.js
 import dotenv from 'dotenv';
 import mysql from 'mysql2/promise';
 
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'sibongadental',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 4000,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
 
 export default pool;
